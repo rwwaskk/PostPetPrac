20cadaeaee4af7703c5f653e18be4438c49422e2
rwwaskk <tsubomi1989@gmail.com>
2012-07-27 16:20:29 -0700

    show following and followed usrs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,14 +20,14 @@ end
 def following
     @title = "Following"
     @user = User.find(params[:id])
-    @users = @user.followed_users.paginate(page: params[:page])
+    @users = @user.followed_users
     render 'show_follow'
   end
   
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers
     render 'show_follow'
   end
 
